7153d5a3311fe7d1636b7983fe1080bd5fb93340
kietkag9-droid <kietkag9@gmail.com>
2026-05-09 16:45:17 +0000

    15

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import gameAnimationImage from './assets/game-animation.svg';
 import paperCutSceneImage from './assets/paper-cut-wolf.svg';
 import paperCupImage from './assets/paper-cup-image.png';
+import gameCharacterImage from './assets/game-character-design.png';
 
 const avatarSrc = 'https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-6/583482867_1348521423429910_7073331310559896751_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=53a332&_nc_eui2=AeGnMRDGC2uWozPLwZoPfpOloZoJrGYcnm6hmgmsZhyeblIvgvqrsaotC7oBTO9QryoQGjC2r_zsS82Mzw6ioGuP&_nc_ohc=ppdjprCO0iIQ7kNvwEKZeJT&_nc_oc=AdpamJzJJOleH79HLL9WYTQUl91RLD38thZsDFRXfTZMn2ANs7Ue74pYSYyyJ9ye-f4&_nc_zt=23&_nc_ht=scontent.fsgn2-5.fna&_nc_gid=Rgf4tECttCx1714fVJC_Aw&_nc_ss=7b2a8&oh=00_Af4iO6FjGkAvMKOs5FCyH8xTF5MiD-eElF91328vBHagJg&oe=69FF7A89';
 
@@ -17,8 +18,9 @@ const siteData = {
     { label: 'Thiết kế 3D giả', level: 78, color: '#70B0FF' },
   ],
   projects: [
-    { title: 'Paper Cup', description: 'Thiết kế thương hiệu Paper Cup với cảm hứng tĩnh lặng và tông xanh đêm.', tags: ['UI', 'Brand', 'Motion'], image: paperCupImage },
-    { title: 'Thiết kế animation game', description: 'Thiết kế giao diện game với phong cách pixel art động và lôi cuốn người chơi.', tags: ['Game', 'Animation', 'Pixel'], image: gameAnimationImage },
+    { title: 'DINH ĐỘC LẬP', description: 'Thiết kế thương hiệu Paper Cup với cảm hứng tĩnh lặng và tông xanh đêm.', tags: ['UI', 'Brand', 'Motion'], image: paperCupImage },
+    { title: 'THIẾT KẾ GIAO DIỆN WEBSITE', description: 'Thiết kế giao diện game với phong cách pixel art động và lôi cuốn người chơi.', tags: ['Game', 'Animation', 'Pixel'], image: gameAnimationImage },
+    { title: 'THIẾT KẾ NHÂN VẬT GAME', description: 'Thiết kế nhân vật game với phong cách độc đáo và chi tiết cao.', tags: ['Game', 'Character', 'Design'], image: gameCharacterImage },
   ],
   experience: [
     { year: '2025', role: 'Senior Designer', company: 'Lava Studio', detail: 'Điều phối dự án trải nghiệm số cho khách hàng cao cấp.' },
